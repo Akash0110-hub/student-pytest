29e55dcdded7705b238d69379e60cc42e638dc91
Akash0110-hub <akashgshettar10@gmail.com>
2025-12-02 11:42:30 +0530

    Create student.py

---
+++ student.py
@@ -0,0 +1,7 @@
+def student_registration(student_id, student_name, course_enrolled, academic_year):
+    return (
+        f"Student ID       : {student_id}\n"
+        f"Student Name     : {student_name}\n"
+        f"Course Enrolled  : {course_enrolled}\n"
+        f"Academic Year    : {academic_year}"
+    )
